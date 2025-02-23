56c248264ba48076a6598e472da3bc3cdb34d95f
wunhand <davidewcasey@gmail.com>
2025-02-23 05:38:54 -0800

    push UBUNTU folder

---
+++ UBUNTU/reset_password.sh
@@ -0,0 +1,10 @@
+#Steps To Reset Password
+
+#1. Open the terminal
+#2. Type the following command:
+mount -rw -o remount /
+#3. Type the following command:
+ls /home
+#4. Type the following command:
+passwd
+
